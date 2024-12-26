bea76b33f9009c12148cb92a6bda0e04311ba8fc
Churilin <vladislav.churilin@gmail.com>
2024-12-26 19:48:19 +0300

    1

--- src/lesson1/Main.java
+++ src/lesson1/Main.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // Задание 1
-        System.out.println("Задание 1");
+        System.out.println("Task 1");
 
         System.out.println("Я \nХорошо \nЗнаю \nJava.");
 
         // Задание 2
-        System.out.println("Задание 2");
+        System.out.println("Task 2");
 
         double res = (46 + 10) * (10 / 3d);
         System.out.println(res);
@@ -18,7 +18,7 @@ public class Main {
         System.out.println(res1);
 
         // Задание 3
-        System.out.println("Задание 3");
+        System.out.println("Task 3");
 
         double number = 10500;
         double result = (number / 10) / 10;
@@ -26,7 +26,7 @@ public class Main {
         System.out.println("Результат с переменной number: " + result);
 
         // Задание 4
-        System.out.println("Задание 4");
+        System.out.println("Task 4");
 
         double a = 3.6;
         double b = 4.1;
@@ -35,6 +35,19 @@ public class Main {
         double result1 = a * b * c;
 
         System.out.println("Произведение трех чисел: " + result);
+
+        // Задание 5
+        System.out.println("Task 5");
+
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.hasNextInt();
+        int input = scanner.nextInt();
+        scanner.hasNextInt();
+        int input1 = scanner.nextInt();
+        scanner.hasNextInt();
+        int input2 = scanner.nextInt();
+        System.out.println(input + "\n" + input1 + "\n" + input2);
 
     }
 }
