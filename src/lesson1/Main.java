233de339361bfa776caa718a883f171f09f21d16
Churilin <vladislav.churilin@gmail.com>
2024-12-26 19:00:22 +0300

    1

---
+++ src/lesson1/Main.java
@@ -0,0 +1,13 @@
+package lesson1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Задание 1
+        System.out.println("Задание 1");
+
+        System.out.println("Я \nХорошо \nЗнаю \nJava.");
+
+}
+}
