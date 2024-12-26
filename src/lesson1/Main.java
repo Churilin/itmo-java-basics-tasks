3e92d159de86ea9bfa74bbcd3262476e5ead0197
Churilin <vladislav.churilin@gmail.com>
2024-12-26 19:27:42 +0300

    1

--- src/lesson1/Main.java
+++ src/lesson1/Main.java
@@ -12,10 +12,10 @@ public class Main {
         // Задание 2
         System.out.println("Задание 2");
 
-        double res1 = (46 + 10) * (10 / 3d);
-        System.out.println("Res1: " + res1);
-        int res2 = (29) * (4) * (-15);
-        System.out.println("Res2: " + res2);
+        double res = (46 + 10) * (10 / 3d);
+        System.out.println(res);
+        int res1 = (29) * (4) * (-15);
+        System.out.println(res1);
 
         // Задание 3
         System.out.println("Задание 3");
@@ -23,7 +23,18 @@ public class Main {
         double number = 10500;
         double result = (number / 10) / 10;
 
-        System.out.println("Результат выражения с переменной number: " + result);
+        System.out.println("Результат с переменной number: " + result);
 
-}
+        // Задание 4
+        System.out.println("Задание 4");
+
+        double a = 3.6;
+        double b = 4.1;
+        double c = 5.9;
+
+        double result1 = a * b * c;
+
+        System.out.println("Произведение трех чисел: " + result);
+
+    }
 }
