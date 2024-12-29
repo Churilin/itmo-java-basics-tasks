4f5e7a971a56734743d1f27cd47cc09c0d966b1c
Churilin <vladislav.churilin@gmail.com>
2024-12-29 16:21:16 +0300

    Created method Main

--- src/lesson2/Calculator.java
+++ src/lesson2/Calculator.java
@@ -1,5 +1,7 @@
 package lesson2;
 
+import java.util.Scanner;
+
 public class Calculator {
     // Сумма
     public static int sum(int a, int b) {return a + b;}
@@ -17,4 +19,29 @@ public class Calculator {
     public static int subtraction(int a, int b) {return a - b;}
     public static double subtraction(double a, double b) {return a - b;}
     public static long subtraction(long a, long b) {return a - b;}
+
+    public static void  main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите два числа для их сложения:");
+        double sum1 = scanner.nextDouble();
+        double sum2 = scanner.nextDouble();
+        System.out.println("Сумма чисел: " + Calculator.sum(sum1, sum2));
+
+        System.out.println("Введите два числа для их деления:");
+        double div1 = scanner.nextDouble();
+        double div2 = scanner.nextDouble();
+        System.out.println("Результат деления: " + Calculator.division(div1, div2));
+
+        System.out.println("Введите два числа для их умножения:");
+        double mult1 = scanner.nextDouble();
+        double mult2 = scanner.nextDouble();
+        System.out.println("Результат умножения: " + Calculator.multiplication(mult1, mult2));
+
+        System.out.println("Введите два числа для вычисления результата разности чисел:");
+        double sub1 = scanner.nextDouble();
+        double sub2 = scanner.nextDouble();
+        System.out.println("Результат вычетания: " + Calculator.subtraction(sub1, sub2));
+    }
 }
