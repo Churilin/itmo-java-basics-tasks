4c9c776a40506a00f7d5172ad676321a565dd6f7
Churilin <vladislav.churilin@gmail.com>
2025-01-04 19:43:56 +0300

    lesson4.TasksPart1, created class sumTwoInt

---
+++ src/lesson4/TasksPart1/sumTwoInt.java
@@ -0,0 +1,29 @@
+package lesson4.TasksPart1;
+
+// Напишите программу, чтобы вычислить сумму двух целых чисел и вернуть true, если сумма равна третьему целому числу
+
+import java.util.Scanner;
+
+public class sumTwoInt {
+    public static void main(String[] args) {
+        checkSumEqInt();
+    }
+
+    static void checkSumEqInt() {
+        System.out.println("Введите первое число:");
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+
+        System.out.println("Введите второе число:");
+        int num2 = scanner.nextInt();
+
+        System.out.println("Введите третье число:");
+        int num3 = scanner.nextInt();
+
+        System.out.println("Результат: " + check(num1, num2, num3));
+    }
+
+    public static Boolean check(int firstNum, int secondNum, int thirdNum) {
+        return firstNum + secondNum == thirdNum;
+    }
+}
