dbfd2ef6ded6f3fa73f93ad6b6814376f7697fb6
Churilin <vladislav.churilin@gmail.com>
2025-01-08 18:39:34 +0300

    lesson4.TasksPart2, created a method for finding the longest word

---
+++ src/lesson5/longWord.java
@@ -0,0 +1,25 @@
+package lesson5;
+
+// Написать метод для поиска самого длинного слова в тексте.
+
+public class longWord {
+
+    public static void main(String[] args) {
+        String text = "Разнообразный и богатый опыт говорит нам, " +
+                "что укрепление и развитие внутренней структуры предоставляет широкие возможности для системы обучения кадров, " +
+                "соответствующей насущным потребностям.";
+        String longestWord = longestWord(text);
+        System.out.println("Самое длинное слово в тексте - это: " + longestWord);
+    }
+
+    public static String longestWord(String text) {
+        String[] words = text.split("\\s+");
+        String longestWord = "";
+        for (String word : words) {
+            if (word.length() > longestWord.length()) {
+                longestWord = word;
+            }
+        }
+        return longestWord;
+    }
+}
