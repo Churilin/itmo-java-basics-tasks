2adfed15715101dae3c218054095dcb40a6b5b75
Churilin <vladislav.churilin@gmail.com>
2025-01-08 19:26:09 +0300

    lesson5, created a method that checks whether a word is a palindrome

---
+++ src/lesson5/wordPalindrome.java
@@ -0,0 +1,28 @@
+package lesson5;
+
+// Написать метод, который проверяет является ли слово палиндромом.
+
+public class wordPalindrome {
+
+    public static void main(String[] args) {
+        String word = "ротор";
+        if (isPalindrome(word)) {
+            System.out.println(word + " - слово палиндром.");
+        } else {
+            System.out.println("слово: " + word + " - не является палиндромом.");
+        }
+    }
+
+    public static boolean isPalindrome(String word) {
+        int left = 0;
+        int right = word.length() - 1;
+        while (left < right) {
+            if (word.charAt(left) != word.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
