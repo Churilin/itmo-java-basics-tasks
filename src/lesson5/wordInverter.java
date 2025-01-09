6fb4e5840e20f405416640fee0d95515b8453c9c
Churilin <vladislav.churilin@gmail.com>
2025-01-09 18:54:16 +0300

    lesson5, created method that inverts words in a string

---
+++ src/lesson5/wordInverter.java
@@ -0,0 +1,23 @@
+package lesson5;
+
+// Напишите метод, который инвертирует слова в строке. Предполагается, что в строке нет знаков препинания, и слова разделены пробелами.
+
+public class wordInverter {
+    public static void main(String[] args) {
+        String inputString = "This is a test string";
+        System.out.println("The given string is: " + inputString + "\n"
+                + "The string reversed word by word is:" + "\n" + reverseWords(inputString));
+    }
+
+    public static String reverseWords(String inputString) {
+        String[] words = inputString.split(" ");
+        StringBuilder invertedString = new StringBuilder();
+
+        for (String word : words) {
+            StringBuilder invertedWord = new StringBuilder(word);
+            invertedString.append(invertedWord.reverse()).append(" ");
+        }
+
+        return invertedString.toString();
+    }
+}
