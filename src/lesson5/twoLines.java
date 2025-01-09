d8fb1c40a762ce258e0a007f39b26df8fde33ab5
Churilin <vladislav.churilin@gmail.com>
2025-01-09 17:10:41 +0300

    lesson5, finding the number of occurrences

---
+++ src/lesson5/twoLines.java
@@ -0,0 +1,41 @@
+package lesson5;
+
+// Имеются две строки. Найти количество вхождений одной (являющейся подстрокой) в другую.
+
+public class twoLines {
+
+    public static void main(String[] args) {
+        String mainStr = "Наши поезда — самые поездатые поезда в мире, " +
+                "и даже самые поездатые поезда не перепоездят наши поезда по поездатости.";
+        String subStr = "поезд";
+
+        int occurrences = getNumberOfOcc(mainStr, subStr);
+
+        System.out.println("В этой строке: " + mainStr);
+        System.out.println("Количество вхождений подстроки: " + "\"" + subStr + "\" " + "- "  + occurrences);
+    }
+
+    public static int getNumberOfOcc(String mainStr, String subStr) {
+        if (mainStr.length() < subStr.length()) {
+            System.out.println("Строка mainStr должна быть не меньше subStr!");
+            return 0;
+        }
+
+        mainStr = mainStr.toLowerCase();
+        subStr = subStr.toLowerCase();
+
+        int number = 0;
+        int index = -1;
+
+        while (index < mainStr.length()) {
+            if (mainStr.indexOf(subStr, index + 1) < 0) {
+                return number;
+            } else {
+                number++;
+                index = mainStr.indexOf(subStr, index + 1);
+            }
+        }
+
+        return number;
+    }
+}
