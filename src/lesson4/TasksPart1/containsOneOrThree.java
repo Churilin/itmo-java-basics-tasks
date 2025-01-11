60f195d079ddbbbc3cc84b667901fd50cf06949b
Churilin <vladislav.churilin@gmail.com>
2025-01-11 19:59:29 +0300

    lesson4\TasksPart1, correcting a method containsOneOrThree

--- src/lesson4/TasksPart1/containsOneOrThree.java
+++ src/lesson4/TasksPart1/containsOneOrThree.java
@@ -2,17 +2,11 @@ package lesson4.TasksPart1;
 
 // Напишите программу, чтобы проверить, что массив содержит число 1 или 3
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class containsOneOrThree {
 
-
     public static void main(String[] args) {
-        checkArrayElement();
-    }
-
-    public static void checkArrayElement() {
         Scanner input = new Scanner(System.in);
         System.out.println("Введите длину массива:");
         int size = input.nextInt();
@@ -23,20 +17,20 @@ public class containsOneOrThree {
             for (int i = 0; i < size; i++) {
                 array[i] = input.nextInt();
             }
+
+            boolean result = checkArrayElement(array);
+            System.out.println("Результат: " + result);
         } else {
             System.out.println("Длина массива должна быть больше или равна 2.");
         }
+    }
 
-        System.out.println("Массив: " + Arrays.toString(array));
-
-        boolean containsOneOrThree = false;
-        for (int i = 0; i < size; i++) {
+    public static boolean checkArrayElement(int[] array) {
+        for (int i = 0; i < array.length; i++) {
             if (array[i] == 1 || array[i] == 3) {
-                containsOneOrThree = true;
-                break;
+                return true;
             }
         }
-
-        System.out.println("Результат: " + containsOneOrThree);
+        return false;
     }
 }
