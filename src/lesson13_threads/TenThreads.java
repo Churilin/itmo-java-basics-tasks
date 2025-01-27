c61125e69a60bb83047f3a920cfd132dfeb8129e
Churilin <vladislav.churilin@gmail.com>
2025-01-27 21:47:35 +0300

    Lesson 13, Task1,2, created class TenThreads.

---
+++ src/lesson13_threads/TenThreads.java
@@ -0,0 +1,27 @@
+package lesson13_threads;
+
+// Напишите программу, в которой запускается 10 потоков и каждый из них выводит числа от 0 до 100.
+// Выведете состояние потока перед его запуском, после запуска и во время выполнения.
+
+
+public class TenThreads extends Thread {
+
+    @Override
+    public void run() {
+        System.out.println(getName() + " is " + getState() + " - started");
+
+        for (int i = 0; i <= 100; i++) {
+            System.out.println(getName() + " is " + getState() + " - running " + i);
+        }
+
+        System.out.println(getName() + " is " + getState() + " - finished");
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            TenThreads thread = new TenThreads();
+            System.out.println(thread.getName() + " is " + thread.getState() + " - created");
+            thread.start();
+        }
+    }
+}
