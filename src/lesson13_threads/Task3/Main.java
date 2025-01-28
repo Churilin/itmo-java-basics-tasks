d46a79040afd7641929b61a70313cfd261ba0bf4
Churilin <vladislav.churilin@gmail.com>
2025-01-28 17:42:47 +0300

    Lesson 13_threads\Task3 update class Main.

--- src/lesson13_threads/Task3/Main.java
+++ src/lesson13_threads/Task3/Main.java
@@ -1,10 +1,14 @@
 package lesson13_threads.Task3;
 
+import java.util.concurrent.CountDownLatch;
+
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
         Counter counter = new Counter();
+
+        CountDownLatch latch = new CountDownLatch(100);
 
         Thread[] threads = new Thread[100];
 
@@ -13,13 +17,16 @@ public class Main {
                 for (int j = 0; j < 1000; j++) {
                     counter.increment();
                 }
+                latch.countDown();
             });
             threads[i].start();
         }
 
-        for (Thread thread : threads) {
-            thread.join();
-        }
+        latch.await();
+
+//        for (Thread thread : threads) {
+//            thread.join();
+//        }
 
         System.out.println("Final count: " + counter.getCount());
     }
